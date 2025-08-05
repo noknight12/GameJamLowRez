cdf78ea6f870941d8511cefb8ffcfa557a725df4
noknight12 <karlonasa12@gmail.com>
2025-08-05 10:01:07 -0700

    added battle transition animation

--- Assets/EnemyMove.cs
+++ Assets/EnemyMove.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyMove : MonoBehaviour
 {
     public Transform player;
+    public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +14,13 @@ public class EnemyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if ( Vector2.Distance(transform.position, player.position) > 2)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, 0.015f);
+        }
+        else 
+        {
+            animator.Play("Transition");
+        }
     }
 }
