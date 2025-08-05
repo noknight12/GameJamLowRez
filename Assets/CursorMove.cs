ab791d801aea1c240da05890323226303459f98b
noknight12 <karlonasa12@gmail.com>
2025-08-04 22:59:05 -0700

    the player can be moved with the cursor

---
+++ Assets/CursorMove.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CursorMove : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    Vector2 mouseCoords;
+    void Start()
+    {
+        mouseCoords = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseCoords = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        Vector2 playerCoord = transform.position;
+        if (playerCoord != mouseCoords)
+        {
+            Vector2 Dir = mouseCoords - playerCoord;
+            transform.position = Vector2.MoveTowards(playerCoord,mouseCoords, 0.02f);
+        }
+    }
+}
