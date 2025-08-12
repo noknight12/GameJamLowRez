c1f935fa5c962b40d07e7dd6acf8a1f8a502ab63
noknight12 <karlonasa12@gmail.com>
2025-08-12 01:01:55 -0700

    ahr

--- Assets/ScriptableObjects/Ability.cs
+++ Assets/ScriptableObjects/Ability.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Ability", menuName = "Scriptable Objects/Ability")]
 public class Ability : ScriptableObject
 {
-    
+    [SerializeField]
+    string abilityName;
+    [SerializeField]
+    AnimationClip clip;
 }
