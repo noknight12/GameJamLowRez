cc796b7ad9cfc3280061723cee8debceee8bf381
noknight12 <karlonasa12@gmail.com>
2025-08-14 23:01:15 -0700

    inventory display

---
+++ Assets/Scripts/Menu/StartGame.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SceneTransfer()
+    {
+        SceneManager.LoadScene("World");
+    }
+}
